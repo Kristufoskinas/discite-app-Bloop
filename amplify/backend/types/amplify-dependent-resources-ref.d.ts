ab002073399dc29d0167c69a3c9fb8f9a5a45874
Jokas9632 <jokas.jasas@gmail.com>
2022-01-17 23:32:28 +0200

    updated

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "disciteapptestbcd2e1f8": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "analytics": {
+        "disciteapptest": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        }
+    }
+}
